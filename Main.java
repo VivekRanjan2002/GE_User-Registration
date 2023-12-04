fa1ea1dbc16f7837e399c60c3428b7a6ff26023d
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-05 01:39:27 +0530

    Added: method to check firstname with pattern defined

--- Main.java
+++ Main.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to User Management Interface ");
+        User user= new User();
 
+      // UC1 first Name checker
+        user.FirstNameMatcher("Viv"); // matches with pattern
+        user.FirstNameMatcher("vIVek"); // don't match with pattern
+        user.FirstNameMatcher("Vi12"); // don't match with pattern
 
 
     }
---
+++ User.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.regex.Pattern;
+import  java.util.regex.Matcher;
+public class User{
+    /*
+    @desc: Matches firstName input with given pattern where first name
+         is capital and have minimum 3 characters and print the statement accordingly
+     */
+    public void FirstNameMatcher(String firstName) {
+        String regexFirstName = "^[A-Z]([A-Za-z]){2,}";
+        Pattern pattern = Pattern.compile(regexFirstName);
+        Matcher matcher = pattern.matcher(firstName);
+        if (matcher.find()) {
+            System.out.println(firstName+" matches with the Pattern");
+        } else {
+            System.out.println(firstName+" don't match with Pattern");
+        }
+    }
+}
