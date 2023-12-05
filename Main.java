c04f8bc64fb25614bb8118653554cf157dba8b9d
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-05 21:28:33 +0530

    Added: method to validate email with defined pattern

--- Main.java
+++ Main.java
@@ -8,15 +8,22 @@ public class Main {
         User user= new User();
 
       // UC1 first Name checker
-        user.FirstNameMatcher("Viv"); // matches with pattern
-        user.FirstNameMatcher("vIVek"); // don't match with pattern
-        user.FirstNameMatcher("Vi12"); // don't match with pattern
+        user.FirstNameMatcher("Viv"); // matches with pattern      true
+        user.FirstNameMatcher("vIVek"); // don't match with pattern   false
+        user.FirstNameMatcher("Vi12"); // don't match with pattern    false
 
         // UC2 Last Name checker
-        user.LastNameMatcher("Ranjan"); // matches with pattern
-        user.LastNameMatcher("rANJAN"); // don't match with pattern
-        user.LastNameMatcher("Rn1"); // don't match with pattern
-        user.LastNameMatcher("Ra"); // don't match with pattern
+        user.LastNameMatcher("Ranjan"); // matches with pattern   true
+        user.LastNameMatcher("rANJAN"); // don't match with pattern   false
+        user.LastNameMatcher("Rn1"); // don't match with pattern       false
+        user.LastNameMatcher("Ra"); // don't match with pattern         false
+
+        //UC3 Email Checker
+         user.EmailMatcher("abc.100@yahoo.com");       // true
+         user.EmailMatcher("abc@1.com");              // true
+         user.EmailMatcher("abc+100@gmail.com");      // true
+         user.EmailMatcher("abc..123@gmail.com");     // false
+
 
     }
 }
--- User.java
+++ User.java
@@ -4,31 +4,39 @@ import  java.util.regex.Matcher;
 public class User{
     /*
     @desc: Matches firstName input with given pattern where first name
-         is capital and have minimum 3 characters and print the statement accordingly
+         is capital and have minimum 3 characters
+    @params: string firstname given by user
+    @return: boolean if matches then 1 and 0 otherwise
      */
-    public void FirstNameMatcher(String firstName) {
+    public boolean FirstNameMatcher(String firstName) {
         String regexFirstName = "^[A-Z]([A-Za-z]){2,}";
         Pattern pattern = Pattern.compile(regexFirstName);
         Matcher matcher = pattern.matcher(firstName);
-        if (matcher.find()) {
-            System.out.println(firstName+" matches with the Pattern");
-        } else {
-            System.out.println(firstName+" don't match with Pattern");
-        }
+        return matcher.find();
     }
 
     /*
     @desc: Matches lastName input with given pattern where last name
-         is capital and have minimum 3 characters and print the statement accordingly
+         is capital and have minimum 3 characters
+     @params: string lastname given by user
+     @return : boolean value true if matches and false if not matches
      */
-    public void LastNameMatcher(String lastName) {
-        String regexlastName = "^[A-Z]([A-Za-z]){2,}";
-        Pattern pattern = Pattern.compile(regexlastName);
-        Matcher matcher = pattern.matcher(lastName);
-        if (matcher.find()) {
-            System.out.println(lastName+" matches with the Pattern");
-        } else {
-            System.out.println(lastName+" don't match with Pattern");
-        }
+    public boolean LastNameMatcher(String lastName) {
+       return FirstNameMatcher(lastName);
+    }
+
+    /*
+    @desc: matches with given email with pattern which have E.g. abc.xyz@bl.co.in has 3 mandatory parts (abc, bl
+            & co) and 2 optional (xyz & in) with precise @ and . positions
+    @params: string as email given by user
+    @return: boolean value if matches then true otherwise false
+     */
+
+    public boolean EmailMatcher(String email){
+        String regexEmail= "^[a-zA-z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([._%+-][a-zA-Z]{2,})+$";
+        Pattern pattern = Pattern.compile(regexEmail);
+        Matcher matcher= pattern.matcher(email);
+       return matcher.find();
+
     }
 }
